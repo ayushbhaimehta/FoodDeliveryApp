308291e7846dc7a3a0d229744153c3cad4fa56bd
anujawas <awasthianuj394@gmail.com>
2024-01-26 22:56:18 +0530

    loader contect added

--- client/App.js
+++ client/App.js
@@ -1,10 +1,14 @@
-import { AuthProvider } from "./AuthContext";
+import { AuthProvider, useAuth } from "./features/context/AuthContext";
 import { HomeNavigation, SignUpNavigation } from "./Navigation";
+import { LoaderContextProvider } from "./features/context/loaderContext";
 
 export default function App() {
+  const { user } = useAuth()
   return (
     <AuthProvider>
-      <SignUpNavigation />
+      <LoaderContextProvider>
+        {user ? <HomeNavigation /> : <SignUpNavigation />}
+      </LoaderContextProvider>
     </AuthProvider>
   );
 }
