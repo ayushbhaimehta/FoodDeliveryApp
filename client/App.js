1e4426a71282a7a0133d2cd4a9fed23dc3a015ec
anujawas <awasthianuj394@gmail.com>
2024-01-27 21:18:43 +0530

    Made Stack dynamic

--- client/App.js
+++ client/App.js
@@ -1,13 +1,12 @@
 import { AuthProvider, useAuth } from "./features/context/AuthContext";
-import { HomeNavigation, SignUpNavigation } from "./Navigation";
+import { Navigation } from "./Navigation";
 import { LoaderContextProvider } from "./features/context/loaderContext";
 
 export default function App() {
-  const { user } = useAuth()
   return (
     <AuthProvider>
       <LoaderContextProvider>
-        {user ? <HomeNavigation /> : <SignUpNavigation />}
+        <Navigation />
       </LoaderContextProvider>
     </AuthProvider>
   );
