444e159a854496e41a90c1f5d4f87b9f06b5d165
anujawas <awasthianuj394@gmail.com>
2024-01-27 21:19:55 +0530

    Made dynamic routing

--- client/Navigation.js
+++ client/Navigation.js
@@ -13,13 +13,15 @@ import SignUpScreen from './screens/SignUp/SignUpScreen';
 import OTPScreen from './screens/SignUp/OTPScreen';
 import NameEmailInput from './screens/SignUp/NameEmailScreen';
 import Address from './screens/SignUp/Address';
+import { useAuth } from './features/context/AuthContext';
 
 const HomeStack = createNativeStackNavigator();
 const SignUpStack = createNativeStackNavigator();
-export const SignUpNavigation = () => {
+export const Navigation = () => {
+    const { user } = useAuth();
     return (
         <NavigationContainer>
-            <Provider store={store}>
+            <Provider store={store}>{!user ?
                 <SignUpStack.Navigator initialRouteName='Address'>
                     <SignUpStack.Screen name="Login" component={SignUpScreen}
                         options={{ headerShown: false }} />
@@ -29,17 +31,8 @@ export const SignUpNavigation = () => {
                         options={{ headerShown: false }} />
                     <SignUpStack.Screen name="Address" component={Address}
                         options={{ headerShown: false }} />
-
                 </SignUpStack.Navigator>
-            </Provider>
-        </NavigationContainer>
-    );
-}
-
-export const HomeNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Provider store={store}>
+                :
                 <HomeStack.Navigator>
                     <HomeStack.Screen name="Home" component={HomeScreen}
                         options={{ headerShown: false }} />
@@ -52,6 +45,7 @@ export const HomeNavigation = () => {
                     <HomeStack.Screen name="Delivery" component={DeliveryScreen}
                         options={{ headerShown: false }} />
                 </HomeStack.Navigator>
+            }
             </Provider>
         </NavigationContainer>
     );
