f4b401983c79d6f64e3ba6b1498e88bb7a9a8203
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2024-01-27 23:14:27 +0530

    dsg

--- server/controllers/orderService/order.controller.js
+++ server/controllers/orderService/order.controller.js
@@ -28,21 +28,41 @@ async function assignOrdersController(req, res) {
 
 async function paymentController(req, res) {
     let orderInfo = req.body;
-    let { error } = orderValidator.validateAddOrderSchema(orderInfo);
-    if (isNotValidSchema(error, res)) return;
+    // let { error } = orderValidator.validateAddOrderSchema(orderInfo);
+    // if (isNotValidSchema(error, res)) return;
     log.success('Schema Validation done');
     const phoneNo = req.phoneNo;
-    var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
-
-    instance.orders.create({
+    const Key_Id = process.env.Key_Id;
+    const Key_Secret = process.env.Key_Secret;
+    const razorpay = new Razorpay({
+        key_id: Key_Id,
+        key_secret: Key_Secret,
+    });
+    const options = {
         amount: 50000,
-        currency: "INR",
-        receipt: "receipt#1",
-        notes: {
-            key1: "value3",
-            key2: "value2"
-        }
-    })
+        currency: 'INR',
+        receipt: "any unique id for every order",
+        payment_capture: 1
+    };
+    // instance.orders.create({
+    //     amount: 50000,
+    //     currency: "INR",
+    //     receipt: "receipt#1",
+    //     notes: {
+    //         key1: "value3",
+    //         key2: "value2"
+    //     }
+    // })
+    try {
+        const response = await razorpay.orders.create(options)
+        res.json({
+            order_id: response.id,
+            currency: response.currency,
+            amount: response.amount,
+        })
+    } catch (err) {
+        res.status(400).send('Not able to create order. Please try again!');
+    }
 }
 
 async function getByUserIdController(req, res) {
