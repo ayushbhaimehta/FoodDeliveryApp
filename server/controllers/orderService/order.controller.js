0616c73d7fc74059747ef5b0b6b544e2062d1db1
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2024-01-29 00:09:07 +0530

    payment completed and so does the whole backend

--- server/controllers/orderService/order.controller.js
+++ server/controllers/orderService/order.controller.js
@@ -5,6 +5,7 @@ const log = new Logger('orderController');
 const { isNotValidSchema } = require('../../utils/notValid.js');
 const { UserModel } = require('../../models/userSchema/user.schemaModel.js');
 const Razorpay = require('razorpay');
+const crypto = require('crypto');
 
 async function addOrderController(req, res) {
     let orderInfo = req.body;
@@ -26,6 +27,7 @@ async function assignOrdersController(req, res) {
     return result;
 }
 
+
 async function paymentController(req, res) {
     let orderInfo = req.body;
     // let { error } = orderValidator.validateAddOrderSchema(orderInfo);
@@ -34,34 +36,50 @@ async function paymentController(req, res) {
     const phoneNo = req.phoneNo;
     const Key_Id = process.env.Key_Id;
     const Key_Secret = process.env.Key_Secret;
-    const razorpay = new Razorpay({
-        key_id: Key_Id,
-        key_secret: Key_Secret,
-    });
-    const options = {
-        amount: 50000,
-        currency: 'INR',
-        receipt: "any unique id for every order",
-        payment_capture: 1
-    };
-    // instance.orders.create({
-    //     amount: 50000,
-    //     currency: "INR",
-    //     receipt: "receipt#1",
-    //     notes: {
-    //         key1: "value3",
-    //         key2: "value2"
-    //     }
-    // })
+    const amount = req.body.amount;
     try {
-        const response = await razorpay.orders.create(options)
-        res.json({
-            order_id: response.id,
-            currency: response.currency,
-            amount: response.amount,
-        })
-    } catch (err) {
-        res.status(400).send('Not able to create order. Please try again!');
+        const instance = new Razorpay({
+            key_id: Key_Id,
+            key_secret: Key_Secret,
+        });
+
+        const options = {
+            amount: amount * 100,
+            currency: "INR",
+            receipt: crypto.randomBytes(10).toString("hex"),
+        };
+
+        instance.orders.create(options, (error, order) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ message: "Something Went Wrong!" });
+            }
+            return res.status(200).json({ data: order });
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error!" });
+    }
+}
+
+async function paymentVerifyController(req, res) {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+            req.body;
+        const sign = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSign = crypto
+            .createHmac("sha256", process.env.KEY_SECRET)
+            .update(sign.toString())
+            .digest("hex");
+
+        if (razorpay_signature === expectedSign) {
+            return res.status(200).json({ message: "Payment verified successfully" });
+        } else {
+            return res.status(400).json({ message: "Invalid signature sent!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!" });
+        console.log(error);
     }
 }
 
@@ -93,5 +111,6 @@ module.exports = {
     deleteOrderController,
     getByUserIdController,
     assignOrdersController,
-    paymentController
+    paymentController,
+    paymentVerifyController
 };
