04744fae291aa768fab799983bc8723ce7edde01
anujawas <awasthianuj394@gmail.com>
2024-01-28 21:53:53 +0530

    Added address form

--- client/components/Address/AddressForm.js
+++ client/components/Address/AddressForm.js
@@ -1,93 +1,131 @@
 // AddressForm.js
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, Button, Text, RadioButton } from 'react-native-paper';
-import RadioButtonGroup from 'react-native-paper/src/components/RadioButton/RadioButtonGroup';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { TextInput, Button, Text } from 'react-native-paper';
+import SaveAsScreen from './SaveAs';
+import { useAuth } from '../../features/context/AuthContext';
+import axios from 'axios';
 
-const AddressForm = ({ onSubmit }) => {
-    const [street, setStreet] = useState('');
-    const [city, setCity] = useState('');
-    const [zipCode, setZipCode] = useState('');
-    const [country, setCountry] = useState('');
+const AddressForm = ({ onSubmit, setApiData, currentLocation, fullAddress }) => {
+    const [houseNo, setHouseNo] = useState('');
+    const [area, setArea] = useState('');
+    const [directions, setDirections] = useState('');
+    const [phoneNo, setPhoneNo] = useState('');
+    const [name, setName] = useState('');
+    const [saveAs, setSaveAs] = useState('');
+    const [myself, setMyself] = useState(true);
+    const { phoneNumber, auth } = useAuth()
+
+    const getUser = async (phoneNumber) => {
+        try {
+            const res = await axios.get(`http://192.168.1.5:3000/user/getbyphone/${phoneNumber}`, {
+                headers: {
+                    auth: auth,
+                    "Content-Type": 'application/json'
+                }
+            });
+            if (res.status === 200) {
+                setName(res.data.result["name"]);
+            } else {
+                console.log("Non-200 status code:", res.status);
+            }
+        } catch (err) {
+            console.log("Error:", err);
+        }
+    }
+
+    useEffect(() => {
+        // console.log(saveAs, name, myself, houseNo, area, phoneNo, directions);
+
+        if (myself) {
+            if (saveAs !== 'Other') {
+                getUser(phoneNumber);
+            }
+            setPhoneNo(phoneNumber)
+        }
+        setApiData({
+            "address": {
+                "name": name,
+                "phoneNo": phoneNo,
+                "myself": myself,
+                "saveAs": saveAs,
+                "houseNo": houseNo,
+                "area": area + ", " + fullAddress,
+                "directions": directions,
+                "location": {
+                    "coordinates": [currentLocation.lat.toString(), currentLocation.lon.toString()]
+                }
+            }
+        })
+    }, [saveAs, name, phoneNo])
 
     const handleSubmit = () => {
-        // Combine the form fields into a complete address
-        const completeAddress = `${street}, ${city}, ${zipCode}, ${country}`;
-
-        // Call the onSubmit prop with the complete address
-        onSubmit(completeAddress);
-    };
-    const [selectedOption, setSelectedOption] = useState('');
-
-    const handleOptionSelect = (option) => {
-        setSelectedOption(option);
-        onSelect(option);
+        onSubmit()
     };
     return (
-        <View style={styles.container}>
-            <View className="border-2 border-[#d6bba1] bg-[#fffdf4] p-2 rounded-md my-3">
-                <Text className="text-[#a78565] font-semibold">
-                    A detailed address will help our Delivery Partner reach your doorstep easily
+        <KeyboardAvoidingView className=""
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+        >
+            <ScrollView
+                automaticallyAdjustKeyboardInsets={true}
+            >
+                <View className="border-2 border-[#d6bba1] bg-[#fffdf4] p-2 rounded-md my-3">
+                    <Text className="text-[#a78565] font-semibold">
+                        A detailed address will help our Delivery Partner reach your doorstep easily
+                    </Text>
+                </View>
+                <TextInput
+                    label="HOUSE/FLAT /BLOCK NO.*"
+                    value={houseNo}
+                    onChangeText={(text) => setHouseNo(text)}
+                    className="bg-white mt-2"
+                    underlineColorAndroid={' rgb(107 114 128 )'}
+                    activeUnderlineColor='#c46e47'
+                />
+                <TextInput
+                    label="APARTMENT /ROAD /AREA"
+                    value={area}
+                    className="bg-white my-2"
+                    onChangeText={(text) => setArea(text)}
+                    underlineColorAndroid={' rgb(107 114 128 )'}
+                    activeUnderlineColor='#c46e47'
+                />
+                <Text className=" text-sm text-gray-600 my-2 left-4">
+                    DIRECTION TO REACH{' '}
+                    <Text className="text-gray-400">
+                        (Optional)
+                    </Text>
                 </Text>
-            </View>
-            <TextInput
-                label="HOUSE/FLAT /BLOCK NO.*"
-                value={street}
-                onChangeText={(text) => setStreet(text)}
-                className="bg-white my-4"
-                underlineColorAndroid={' rgb(107 114 128 )'}
-                activeUnderlineColor='#c46e47'
-            />
-            <TextInput
-                label="APARTMENT /ROAD /AREA"
-                value={city}
-                className="bg-white my-2"
-                onChangeText={(text) => setCity(text)}
-            />
-            <Text className=" text-base text-gray-600 mb-2 left-4">
-                DIRECTION TO REACH{' '}
-                <Text className="text-gray-400">
-                    (Optional)
-                </Text>
-            </Text>
-            <TextInput
-                placeholder='e.g. Ring the bell on the red gate'
-                className="bg-gray-200 h-32 justify-start align-top rounded-md"
-            />
-            <View style={{ padding: 16, flexDirection: 'row' }}>
-                <RadioButtonGroup onValueChange={(value) => handleOptionSelect(value)} value={selectedOption}
-                    style={{
+                <TextInput
 
-                    }}
-                >
-                    <View style={styles.optionContainer}>
-                        <RadioButton value="home" />
-                        <Text>Home</Text>
-                    </View>
-                    <View style={styles.optionContainer}>
-                        <RadioButton value="work" />
-                        <Text>Work</Text>
-                    </View>
-                    <View style={styles.optionContainer}>
-                        <RadioButton value="friends" />
-                        <Text>Friends</Text>
-                    </View>
-                    <View style={styles.optionContainer}>
-                        <RadioButton value="family" />
-                        <Text>Family</Text>
-                    </View>
-                    <View style={styles.optionContainer}>
-                        <RadioButton value="others" />
-                        <Text>Others</Text>
-                    </View>
-                </RadioButtonGroup>
-            </View>
-            <Button mode='elevated' onPress={handleSubmit} style={styles.Button} >
-                <Text className="text-white text-lg">
-                    Submit
-                </Text>
-            </Button>
-        </View>
+                    placeholder='e.g. Ring the bell on the red gate'
+                    onChangeText={(text) => setDirections(text)}
+                    value={directions}
+                    className="bg-gray-200 h-[80] justify-start align-top rounded-md"
+                    underlineColorAndroid={' rgb(107 114 128 )'}
+                    activeUnderlineColor='#c46e47'
+                />
+                <SaveAsScreen
+                    setMyself={setMyself}
+                    setSaveAs={setSaveAs}
+                    setName={setName}
+                    name={name}
+                    phoneNo={phoneNo}
+                    setPhoneNo={setPhoneNo}
+                />
+                <Button mode='elevated' onPress={handleSubmit} style={styles.Button} >
+                    <Text className="text-white text-lg">
+                        Submit
+                    </Text>
+                </Button>
+                <View className="h-[30]">
+
+                </View>
+            </ScrollView>
+
+        </KeyboardAvoidingView>
+
     );
 };
 
@@ -95,7 +133,7 @@ const styles = StyleSheet.create({
     container: {
         padding: 16,
         backgroundColor: 'white',
-        position: 'absolute'
+        flex: 1
     },
     Button: {
         marginTop: 20,
