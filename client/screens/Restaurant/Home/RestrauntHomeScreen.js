c7f923067ed48ac2f55361c126fa78b782c63164
anujawas <awasthianuj394@gmail.com>
2024-02-12 22:32:28 +0530

    Home screen for restaurants

---
+++ client/screens/Restaurant/Home/RestrauntHomeScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const RestrauntHomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text></Text>
+        </SafeAreaView>
+    )
+}
+
+export default RestrauntHomeScreen
+
+const styles = StyleSheet.create({})
