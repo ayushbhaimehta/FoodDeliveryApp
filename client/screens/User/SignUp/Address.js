153afeeee4733457cc700c83370422e9d8299663
anujawas <awasthianuj394@gmail.com>
2024-02-19 02:41:45 +0530

    Address form bug fixed

--- client/screens/User/SignUp/Address.js
+++ client/screens/User/SignUp/Address.js
@@ -25,7 +25,7 @@ const Address = ({ navigation }) => {
     });
     const [apiData, setApiData] = useState({})
 
-    const [isFormVisible, setIsFormVisible] = useState(true);
+    const [isFormVisible, setIsFormVisible] = useState(false);
     const [fullAddress, setFullAddress] = useState("");
     const [location, setLocation] = useState("")
 
