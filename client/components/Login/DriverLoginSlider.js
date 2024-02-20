ececc43688ce74d3514c352090e2673214d45139
anujawas <awasthianuj394@gmail.com>
2024-02-21 02:41:51 +0530

    Slider finalised

--- client/components/Login/DriverLoginSlider.js
+++ client/components/Login/DriverLoginSlider.js
@@ -4,10 +4,10 @@ import { View, FlatList, Image, StyleSheet, Dimensions, Animated } from 'react-n
 const Slider = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const images = [
-        { id: 1, uri: 'https://via.placeholder.com/600/92c952' },
-        { id: 2, uri: 'https://via.placeholder.com/600/771796' },
-        { id: 3, uri: 'https://via.placeholder.com/600/24f355' },
-        { id: 4, uri: 'https://via.placeholder.com/600/d32776' }
+        { id: 1, uri: 'https://firebasestorage.googleapis.com/v0/b/food-delivery-app-2a7d6.appspot.com/o/data%2Fslide1.jpeg?alt=media&token=52201b0e-e452-4d0e-bf54-a3fcc9b6cdaf' },
+        { id: 2, uri: 'https://firebasestorage.googleapis.com/v0/b/food-delivery-app-2a7d6.appspot.com/o/data%2Fslide2.jpeg?alt=media&token=26f0818e-ce83-4e05-a719-0b8a9222a913' },
+        { id: 3, uri: 'https://firebasestorage.googleapis.com/v0/b/food-delivery-app-2a7d6.appspot.com/o/data%2Fslide3.jpeg?alt=media&token=4c314500-55bd-4fca-9e83-d66b0b5baf00' },
+        { id: 4, uri: 'https://firebasestorage.googleapis.com/v0/b/food-delivery-app-2a7d6.appspot.com/o/data%2Fslide4.jpeg?alt=media&token=51438a30-57f9-4151-b209-793e8f06c1be' }
     ];
     const flatListRef = useRef(null);
 
@@ -33,7 +33,7 @@ const Slider = () => {
                 renderItem={({ item }) => (
                     <Image
                         source={{ uri: item.uri }}
-                        style={{ width: Dimensions.get('window').width, height: 600 }}
+                        style={{ width: Dimensions.get('window').width, height: 600, borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}
                         resizeMode="cover"
                     />
                 )}
@@ -66,6 +66,7 @@ const styles = StyleSheet.create({
         bottom: 20,
         left: 0,
         right: 0,
+        paddingHorizontal: 10
     },
     dot: {
         width: "22%",
